e6c386d41f05d1c71f1c65405cfbf8115cf950be
Robin9087 <44565487+Robin9087@users.noreply.github.com>
2018-10-29 01:08:53 -0700

    Add files via upload

---
+++ MaximalRectangle.java
@@ -0,0 +1,68 @@
+public class MaximalRectangle {
+    private MaximalRectangle(){
+
+    }
+
+    private static int findMaxLen(char[] row) {
+        int size = 0;
+        int tempS = 0;
+        for (char c : row) {
+            if (c == '1') {
+                tempS++;
+            } else {
+                if (tempS > size) {
+                    size = tempS;
+                }
+                tempS = 0;
+            }
+        }
+        if (tempS > size) {
+            size = tempS;
+        }
+        return size;
+    }
+
+    private static int findCurrentMax(char[][] matrix, int n) {
+        int i = n;
+        char[] tempRow = matrix[n];
+        int maxSize = findMaxLen(tempRow);
+        int tempi = 1;
+        while (i < matrix.length - 1) {
+            i++;
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (((tempRow[j] - '0') & (matrix[i][j] - '0')) == 1) {
+                    tempRow[j] = '1';
+                } else {
+                    tempRow[j] = '0';
+                }
+            }
+            tempi++;
+            int tempSize = findMaxLen(tempRow) * tempi;
+            if (maxSize < tempSize) {
+                maxSize = tempSize;
+            }
+        }
+        return maxSize;
+    }
+
+    public static int maximalRectangle(char[][] matrix) {
+        int maxSize = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            int temp = findCurrentMax(matrix, i);
+            if (maxSize < temp) {
+                maxSize = temp;
+            }
+        }
+        return maxSize;
+    }
+
+    public static void main(String[] args) {
+        char[][] matrix = {
+                {'1','0','1','0','0'},
+                {'1','0','1','1','1'},
+                {'1','1','1','1','1'},
+                {'1','0','0','1','0'}
+        };
+        System.out.println(MaximalRectangle.maximalRectangle(matrix));
+    }
+}
